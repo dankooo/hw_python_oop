0be2e76b909faca3fa47f023054d76b28eb08683
Данил Филиппов <sniper9543@yandex.ru>
2020-08-27 17:41:28 +0300

    Third try...

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
-date_format = '%d.%m.%Y'
+DATE_FORMAT = '%d.%m.%Y'
+TODAY = dt.datetime.now()
 
 
 class Record:
@@ -8,73 +9,73 @@ class Record:
     def __init__(self, amount, comment, date = None): 
         self.amount = amount
         if date is None:
-            self.date = dt.datetime.now().date()
+            self.date = TODAY.date()
         else:
-            self.date = dt.datetime.strptime(date, date_format).date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
         self.comment = comment 
 
 class Calculator:
-    today = dt.datetime.now().date()
 
     def __init__(self,limit):
         self.limit = limit
         self.records = []
-        self.remain = 0
 
     def add_record(self, Record):
         self.records.append(Record)
         return self.records
 
     def get_today_stats(self):   
-        self.remain = sum(record.amount for record in self.records if record.date == self.today)
-        return self.remain
+        return sum(
+            record.amount 
+            for record in self.records 
+            if record.date == TODAY.date()
+            )
 
     def get_week_stats(self):
-        week = dt.datetime.now() - dt.timedelta(days = 6)
-        self.remain = sum(record.amount for record in self.records if week.date() <= record.date <= self.today)
-        return self.remain
+        week = TODAY - dt.timedelta(days = 7)
+        return sum(
+            record.amount 
+            for record in self.records 
+            if week.date() <= record.date <= self.TODAY.date()
+            )
 
 class CaloriesCalculator(Calculator):
+    ANSWER = ('Сегодня можно съесть что-нибудь ещё,'
+             'но с общей калорийностью не более {value} кКал')
 
     def get_calories_remained(self):
         self.remain = self.get_today_stats()
         if self.remain < self.limit:
             self.remain = self.limit - self.remain
-            answer = f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.remain} кКал'
+            return self.ANSWER.format(value = self.remain)
         else:
-            answer =  f'Хватит есть!'
-        return answer
+            return 'Хватит есть!'
 
 class CashCalculator(Calculator):
     USD_RATE = 60.0
     EURO_RATE = 70.0
     RUB_RATE = 1.0
+    ANSWER_POS = 'На сегодня осталось {value} {cur}'
+    ANSWER_NEG = 'Денег нет, держись: твой долг - {value} {cur}'
 
     rate_dict = {
-        USD_RATE: 'usd',
-        EURO_RATE: 'eur',
-        RUB_RATE: 'rub'
-    }
-
-    rate_sign = {
-        'usd': 'USD',
-        'eur': 'Euro',
-        'rub': 'руб'
+        'usd':(USD_RATE,'UDS'),
+        'eur':(EURO_RATE,'Euro'),
+        'rub':(RUB_RATE,'руб')
     }
 
     def get_today_cash_remained(self, currency):
-        remained = abs(self.remain - self.get_today_stats())
-        for rate in self.rate_dict:
-            if self.rate_dict[rate] == currency:
-                rate_name = self.rate_sign[self.rate_dict[rate]]
-                remains = round(self.limit / rate - remained / rate, 2)
-                if remained < self.limit:
-                    answer = f'На сегодня осталось {remains} {rate_name}'
-                    return answer
-                elif remained == self.limit:
-                    return 'Денег нет, держись'
-                else:
-                    answer = f'Денег нет, держись: твой долг - {abs(remains)} {rate_name}'
-                    
-                    return answer
-        return 'Неизвестная валюта'
+        remained = self.get_today_stats()
+        rate_name = self.rate_dict[currency][1]
+        remains = round(
+            self.limit / self.rate_dict[currency][0] 
+            - 
+            remained / self.rate_dict[currency][0], 
+            2
+            )
+        if remained < self.limit:
+            return self.ANSWER_POS.format(value = remains, cur = rate_name)
+        elif remained > self.limit:
+            return self.ANSWER_NEG.format(value = abs(remains), cur = rate_name)
+        else:
+            return 'Денег нет, держись'
